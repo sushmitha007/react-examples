07cf237977408de6d3dceb7579b3592625510545
sushmitha <sushmitha@stackroute.in>
2019-04-03 12:17:46 +0530

    props from index.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const App = () => {
-  return <h3>Hello World!Welcome to Stackroute</h3>
+const App = (props) => {
+  return <h3>Hello World!Welcome to {props.name}</h3>
 }
 export default App;
