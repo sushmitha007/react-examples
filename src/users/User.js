0c601d2de7f64235901d1f6cf3d47161d9677897
sushmitha <sushmitha@stackroute.in>
2019-04-04 17:42:49 +0530

    setting state from child component

--- src/users/User.js
+++ src/users/User.js
@@ -1,10 +1,12 @@
-import React from 'react';
-
-const User = (props) => {
+import React, { Component } from 'react';
+class User extends Component {
+    render() {
   return (
         <div>
-        Name: {props.children} | Age: {props.age}
+        <h4>child component</h4>
+        <button onClick = {this.props.handler}>Make me 1 year elder</button>
     </div>)
+}
 }
 
 export default User;
--- src/users/Users.js
+++ src/users/Users.js
@@ -2,41 +2,42 @@ import React, { Component } from 'react';
 import User from './User';
 
 class Users extends Component {
-  // Setting Initial State
-  state = {
-    users: [
-      {name:"John", age:20},
-      {name:"Jill", age:30},
-      {name:"Peter", age:40}
-    ],
-    title:"Users List"
-  }
+    // Setting Initial State
+    state = {
+        users: [
+            { name: "John", age: 20 },
+            { name: "Jill", age: 30 },
+            { name: "Peter", age: 40 }
+        ],
+        title: "Users List"
+    }
 
-  // Handle to modify state
-  makeMeElder = () => {
-    const newState = this.state.users.map((user)=>{
-      const tempUser = user;
-      tempUser.age +=1;
-      return tempUser;
-    });
-    this.setState({
-      newState
-    });
-  }
+    // Handle to modify state
+    makeMeElder = () => {
+        const newState = this.state.users.map((user) => {
+            const tempUser = user;
+            tempUser.age += 1;
+            return tempUser;
+        });
+        this.setState({
+            newState
+        });
+    }
 
-  render(){
-    return (<div>
-      <button onClick={this.makeMeElder}>Make Us 1 year elder</button>
-      <br/>
-      <h1>{this.state.title}</h1>
-      <h3>child Component</h3>
-      {
-        this.state.users.map((user)=>{
-          return <User age={user.age}>{user.name}</User>
-        })
-      }
-      </div>)
-  }
+    render() {
+        return (<div>
+            <div>
+                <h1>{this.state.title}</h1>
+                {
+                    this.state.users.map((user) => {
+                        return <div>Name:{user.name} | Age:{user.age}</div>
+                    })
+                }
+            </div>
+            <User handler={this.makeMeElder}> </User>
+            </div>
+        )
+    }
 }
 
 export default Users;
